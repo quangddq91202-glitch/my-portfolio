ed30a39ce427688594f17047577e0ed5a5622798
quangddq91202-glitch <quangddq91202@gmail.com>
2026-02-03 15:00:24 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Dương Đăng Quang đã sẵn sàng!');
-    // Hiển thị lời chào khi khách truy cập vào trang
-    alert('Chào mừng bạn đến với portfolio của Dương Đăng Quang!');
-});
